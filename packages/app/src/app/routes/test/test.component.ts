dc9b7723b22aeac2a54ddc2a48093377f5b2a934
Steve Black <stevenjblack@gmail.com>
2020-06-28 04:56:49 -0400

    add spinner and buttons to test page

--- packages/app/src/app/routes/test/test.component.ts
+++ packages/app/src/app/routes/test/test.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Host, HostBinding } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { faCoffee, faCircle, faFlag } from '@fortawesome/free-solid-svg-icons';
 
 enum THEMES {
   light = 'light',
@@ -10,18 +11,18 @@ enum THEMES {
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss']
 })
-export class TestComponent implements OnInit {
+export class TestComponent {
   @HostBinding('class.container') container = 'container';
   // tslint:disable-next-line:max-line-length
   lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   // tslint:disable-next-line:max-line-length
   loremLink = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <a href="#">nostrud exercitation</a> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
   theme = 'dark';
+  faCoffee = faCoffee;
+  faCircle = faCircle;
+  faFlag = faFlag;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
   toggleTheme() {
     const htmlEl = document.getElementsByTagName('html')[0];
