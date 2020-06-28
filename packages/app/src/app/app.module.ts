b79a5f16d7cc002b8491ee8f8f923ae437d40ba1
Steve Black <stevenjblack@gmail.com>
2020-06-28 01:55:23 -0400

    create test component for styles

--- packages/app/src/app/app.module.ts
+++ packages/app/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { TestComponent } from './routes/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
---
+++ packages/app/src/app/routes/test/test.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Host, HostBinding } from '@angular/core';
+
+enum THEMES {
+  light = 'light',
+  dark = 'dark',
+}
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent implements OnInit {
+  @HostBinding('class.container') container = 'container';
+  // tslint:disable-next-line:max-line-length
+  lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  // tslint:disable-next-line:max-line-length
+  loremLink = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <a href="#">nostrud exercitation</a> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.';
+  theme = 'dark';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleTheme() {
+    const htmlEl = document.getElementsByTagName('html')[0];
+    const isDark = htmlEl.classList.contains(THEMES.dark);
+    htmlEl.classList[isDark ? 'remove' : 'add'](THEMES.dark);
+  }
+}
