c01b30c664789ca761566caad84199f958520066
Steve Black <stevenjblack@gmail.com>
2020-06-28 05:36:13 -0400

    ThemeService created

---
+++ packages/app/src/app/services/theme.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+
+export enum THEMES {
+  light = 'light',
+  dark = 'dark',
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  lhKey = 'sb:theme';
+  // tslint:disable-next-line:variable-name
+  _theme = THEMES.light;
+  set theme(val: THEMES) {
+    const bodyEl = document.body;
+    this.clearThemes();
+    this._theme = val;
+    bodyEl.classList.add(val);
+    localStorage.setItem(this.lhKey, val);
+  }
+  get theme() {
+    return this._theme;
+  }
+
+  constructor() {
+    this.fetchTheme();
+  }
+
+  fetchTheme() {
+    const theme = localStorage.getItem(this.lhKey);
+
+    if (!theme) {
+      this.theme = THEMES.light;
+      return;
+    }
+
+    this.theme = THEMES[theme];
+  }
+
+  toggleTheme() {
+    this.clearThemes();
+    this.theme = this.theme === THEMES.dark
+      ? THEMES.light
+      : THEMES.dark;
+  }
+
+  clearThemes() {
+    const bodyEl = document.body;
+
+    Object.values(THEMES).forEach(theme => {
+      bodyEl.classList.remove(theme);
+    });
+  }
+}
