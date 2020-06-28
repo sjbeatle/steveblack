bf81307de21c17332bcbba76d215d09edbd8994f
Steven Black <sblack@us.ibm.com>
2020-06-28 13:21:28 -0400

    card component style updates

--- packages/app/src/app/components/card/card.component.ts
+++ packages/app/src/app/components/card/card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
+  static className = 'sb-card';
+  @HostBinding(`class.invert-theme`) invertedTheme = true;
+  @HostBinding(`class.${CardComponent.className}`) sbCard = true;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
