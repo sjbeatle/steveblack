75297c04412e51e3492ea89a6b5d8bffaac5f127
Steve Black <stevenjblack@gmail.com>
2020-11-14 16:59:03 -0500

    update routes

--- packages/app/src/app/app-routing.module.ts
+++ packages/app/src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TestComponent } from './routes/test/test.component';
+import { PerformancesComponent } from './routes/performances/performances.component';
+import { ContactComponent } from './routes/contact/contact.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: TestComponent,
+  },
+  {
+    path: 'performances',
+    component: PerformancesComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
   },
 ];
 
