373523ef9e02087e0743ffd2b5b07e3f968c30a4
Steve Black <stevenjblack@gmail.com>
2020-11-14 16:58:29 -0500

    update header.component

--- packages/app/src/app/components/core/header/header.component.ts
+++ packages/app/src/app/components/core/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { faAdjust } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faYoutube, faPaypal } from '@fortawesome/free-brands-svg-icons';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @HostBinding('class.container-fluid') container = true;
+  faAdjust = faAdjust;
+  faFacebook = faFacebook;
+  faYoutube = faYoutube;
+  faPaypal = faPaypal;
 
-  constructor() { }
+  constructor(public themeService: ThemeService) { }
+
+  get themeLabel() {
+    return `Toggle theme (${this.themeService.theme} currently enabled)`;
+  }
 
   ngOnInit() {
   }
