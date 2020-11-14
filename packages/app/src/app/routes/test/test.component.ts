dbabb1f9b5b4009c4cef52d00b0b3b93c0ec7d46
Steve Black <stevenjblack@gmail.com>
2020-11-14 16:58:03 -0500

    Update test.component.ts

--- packages/app/src/app/routes/test/test.component.ts
+++ packages/app/src/app/routes/test/test.component.ts
@@ -8,7 +8,7 @@ import { ThemeService } from '../../services/theme.service';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent {
-  @HostBinding('class.container') container = 'container';
+  @HostBinding('class.container-fluid') container = 'container';
   // tslint:disable-next-line:max-line-length
   lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   // tslint:disable-next-line:max-line-length
